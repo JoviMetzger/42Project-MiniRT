86be38b4dec3e218cb4d794709a7b622cda520fb
smclacke <sarahmclacken@gmail.com>
2024-03-08 16:06:48 +0100

    added headers, gunna merge with main and see how that goes...

--- src/movement.c
+++ src/movement.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   movement.c                                         :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/03/08 16:05:43 by smclacke      #+#    #+#                 */
+/*   Updated: 2024/03/08 16:05:45 by smclacke      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../header/miniRT.h"
 
 void ft_key_action(mlx_key_data_t keydata, t_data *data)
