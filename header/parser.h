12eff77f68f864d7f6a13cb4569a98636a88b529
smclacke <sarahmclacken@gmail.com>
2024-03-13 00:43:10 +0100

    convert/sort_caps file a mess but getting the logic down, need to work with data struct and do a lot of checking...

--- header/parser.h
+++ header/parser.h
@@ -6,7 +6,7 @@
 /*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/03/08 15:29:06 by smclacke      #+#    #+#                 */
-/*   Updated: 2024/03/12 23:54:20 by smclacke      ########   odam.nl         */
+/*   Updated: 2024/03/13 00:35:00 by smclacke      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -104,6 +104,7 @@ double		ft_atof(char *str);
 
 // --- sort_caps --- //
 int			sort_a(char **elem_str, t_data *data);
+int			sort_l(char **elem_str, t_data *data);
 
 // --- sort_other --- //
 
--- src/parser/convert/sort_caps.c
+++ src/parser/convert/sort_caps.c
@@ -6,7 +6,7 @@
 /*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/03/12 23:42:42 by smclacke      #+#    #+#                 */
-/*   Updated: 2024/03/13 00:28:39 by smclacke      ########   odam.nl         */
+/*   Updated: 2024/03/13 00:41:16 by smclacke      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,13 @@
 int	sort_a(char **elem_str, t_data *data)
 {
 	(void) data;
+
+	/////////// RATIO ///////////
 	double		ratio;
 
 	ratio = ft_atof(elem_str[1]);
-	printf("ratio = %f\n", ratio);
+	printf("A ratio = %f\n", ratio);
+	/////////// RATIO ///////////
 	
 	//////// RGB   ///////////
 	// convert_rgb(elem_str[2]); // create this func when i can use data...
@@ -43,14 +46,71 @@ int	sort_a(char **elem_str, t_data *data)
 
 	
 	rgb = ft_split(elem_str[2], ',');
-	if (!rgb)
-		printf("oopies\n");
+	/// also check we get three strings back
 	r = ft_atoi(rgb[0]);
 	g = ft_atoi(rgb[1]);
 	b = ft_atoi(rgb[2]);
 	free_array(rgb);
-	printf("r = %i, g = %i, b = %i\n", r, g, b);
+	printf("A r = %i, A g = %i, A b = %i\n", r, g, b);
+	//////// RGB   ///////////
 	
+	
+	return (1);
+}
+
+int	sort_l(char **elem_str, t_data *data)
+{
+	// int		i = 0;
+	// while (elem_str[i])
+	// {
+	// 	printf("elem_str = %s\n", elem_str[i]);
+	// 	i++;
+	// }
+	(void) data;
+
+	//////// COORDS ///////////
+	// convert_coords(elem_str[2]); // create this func when i can use data...
+	// t_pos3 struct
+	
+	char	**coords;
+	double	x;
+	double	y;
+	double	z;
+	
+	coords = ft_split(elem_str[1], ',');
+	/// also check we get three strings back
+	x = ft_atof(coords[0]);
+	y = ft_atof(coords[1]);
+	z = ft_atof(coords[2]);
+	free_array(coords);
+	printf("L x = %f, L y = %f, L z = %f\n", x, y, z);
+	//////// COORDS ///////////
+
+
+	/////////// RATIO ///////////
+	double	ratio;
+
+	ratio = ft_atof(elem_str[2]);
+	printf("L ratio = %f\n", ratio);
+	/////////// RATIO ///////////
+
+	//////// RGB   ///////////
+	// convert_rgb(elem_str[2]); // create this func when i can use data...
+	
+	// solution for now for rgb
+	char	**rgb;
+	int			r;
+	int			g;
+	int			b;
+
+	rgb = ft_split(elem_str[3], ',');
+	/// also check we get three strings back
+	r = ft_atoi(rgb[0]);
+	g = ft_atoi(rgb[1]);
+	b = ft_atoi(rgb[2]);
+	free_array(rgb);
+	printf("L r = %i, L g = %i, L b = %i\n", r, g, b);
+	//////// RGB   ///////////
 	
 	return (1);
 }
