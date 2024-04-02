fea86b4d73a0db197235d116a2cd1786f81d292e
smclacke <sarahmclacken@gmail.com>
2024-04-02 20:27:26 +0200

    cylinder weird, i think im missing part of the equation'

--- src/objects/cylinder.c
+++ src/objects/cylinder.c
@@ -6,78 +6,43 @@
 /*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/03/07 19:29:03 by smclacke      #+#    #+#                 */
-/*   Updated: 2024/04/02 17:45:30 by smclacke      ########   odam.nl         */
+/*   Updated: 2024/04/02 20:25:33 by smclacke      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../header/miniRT.h"
 
-/**
- * 
- * t_inter_point ft_cylinder_collision(t_ray ray, t_pack cylinder, int id)
-{
-    t_inter_point inter_point;
-    t_delta delta;
-    t_ray new_ray;
-    t_vect c_to_o;
-
-    inter_point.hit = FALSE;
-    inter_point.id = id;
-    new_ray.origin = ray.origin;
-    cylinder.rot = normalize(cylinder.rot);
-    new_ray.direction = cross(ray.direction, cylinder.rot);
-	
-    c_to_o = sub(ray.origin, cylinder.pos);
-    delta.a = dot(new_ray.direction, new_ray.direction);
-    delta.b = 2 * dot(new_ray.direction, cross(c_to_o, cylinder.rot));
-    delta.c = dot(cross(c_to_o, cylinder.rot), cross(c_to_o, cylinder.rot)) - pow(cylinder.diameter / 2, 2);
-    delta.delta = pow(delta.b, 2) - 4 * delta.c * delta.a;
-   
-    if (delta.delta < 0)
-        return (inter_point);
-    inter_point.t1 = (-delta.b - sqrt(delta.delta)) / (2 * delta.a);
-    inter_point.t2 = (-delta.b + sqrt(delta.delta)) / (2 * delta.a);
-    if (inter_point.t2 < 0)
-        return (inter_point);
-    inter_point.t = (inter_point.t1 > 0 ? inter_point.t1 : inter_point.t2);
-    return (ft_find_edges(cylinder, ray, inter_point));
-}
-*/
-
-// coords, 3D ector, diameter, height, rgb
-// vector for cylinder needs to be used plus height
 bool	intersect_cylinder(t_ray *ray, t_objs *cylinder, t_obj_data *obj_data)
 {
 	t_vec3	oc;
+	double	radius;
 
-	cylinder->center = normalize_vector(cylinder->center);
-	ray->vector = cross_product(ray->vector, cylinder->center);
-	// c_to_o = sub(ray.origin, cylinder.pos);
+	radius = cylinder->diameter / 2;
 	oc = minus(ray->place, cylinder->center);
-	
-    // delta.a = dot(new_ray.direction, new_ray.direction);
-	obj_data->a = dot_product(ray->vector, ray->vector);
-	
-    // delta.b = 2 * dot(new_ray.direction, cross(c_to_o, cylinder.rot));
-	obj_data->b = 2.0 * dot_product(ray->vector, cross_product(oc, cylinder->center));
-	
-    // delta.c = dot(cross(c_to_o, cylinder.rot), cross(c_to_o, cylinder.rot)) - pow(cylinder.diameter / 2, 2);
-	obj_data->c = dot_product(cross_product(oc, cylinder->center), cross_product(oc, cylinder->center)) - pow(cylinder->diameter / 2, 2);
 
-    // delta.delta = pow(delta.b, 2) - 4 * delta.c * delta.a;
-	obj_data->d = pow(obj_data->b, 2) - 4 * obj_data->c * obj_data->a;
+	obj_data->a = (dot_product(ray->vector, ray->vector)) - (pow(dot_product(ray->vector, cylinder->center), 2));
+	obj_data->b = 2.0 * dot_product(oc, ray->vector) - (dot_product(ray->vector, cylinder->center) * dot_product(oc, cylinder->center));
+	obj_data->c = dot_product(oc, oc) - pow(dot_product(oc, cylinder->center), 2) - pow(radius, 2);
 
+	// dont know if this is necessary
+	// if (obj_data->a == 0 && obj_data->b != 0)
+	// {
+	// 	obj_data->root1 = -obj_data->c / obj_data->b;
+	// 	return (true);
+	// }
+	
+	obj_data->d = obj_data->b * obj_data->b - 4 * obj_data->a * obj_data->c;
 	if (obj_data->d < 0)
 		return (false); // no intersection
 	else
 	{
-		obj_data->root1 = (-obj_data->b - sqrt(obj_data->d)) / (2.0 * obj_data->a);
-		obj_data->root2 = (-obj_data->b + sqrt(obj_data->d)) / (2.0 * obj_data->a);
-		if (obj_data->root2 < 0)
-			obj_data->t = obj_data->root2;
+		obj_data->root1 = (-obj_data->b - sqrt(obj_data->d)) / (2 * obj_data->a);
+		obj_data->root2 = (-obj_data->b + sqrt(obj_data->d)) / (2 * obj_data->a);
+		if (obj_data->root1 > obj_data->root2)
+			obj_data->t = obj_data->root1;
 		else
-			obj_data->t = (obj_data->root1 > 0 ? obj_data->root1 : obj_data->root2);
-		return (true); // intersection found
+			obj_data->t = obj_data->root2;
+		return (true); // Intersection found
 	}
 }
 
--- zom_notes/obj_equ.md
+++ zom_notes/obj_equ.md
@@ -53,3 +53,113 @@ pl              1,2,1       0,0,0    255,0,255
 
 white cycle
 pl              2,0,-3       0,0.2,-1    255,255,255
+
+
+/**
+ * 
+ * t_inter_point ft_cylinder_collision(t_ray ray, t_pack cylinder, int id)
+{
+    t_inter_point inter_point;
+    t_delta delta;
+    t_ray new_ray;
+    t_vect c_to_o;
+
+    inter_point.hit = FALSE;
+    inter_point.id = id;
+    new_ray.origin = ray.origin;
+    cylinder.rot = normalize(cylinder.rot);
+    new_ray.direction = cross(ray.direction, cylinder.rot);
+	
+    c_to_o = sub(ray.origin, cylinder.pos);
+    delta.a = dot(new_ray.direction, new_ray.direction);
+    delta.b = 2 * dot(new_ray.direction, cross(c_to_o, cylinder.rot));
+    delta.c = dot(cross(c_to_o, cylinder.rot), cross(c_to_o, cylinder.rot)) - pow(cylinder.diameter / 2, 2);
+    delta.delta = pow(delta.b, 2) - 4 * delta.c * delta.a;
+   
+    if (delta.delta < 0)
+        return (inter_point);
+    inter_point.t1 = (-delta.b - sqrt(delta.delta)) / (2 * delta.a);
+    inter_point.t2 = (-delta.b + sqrt(delta.delta)) / (2 * delta.a);
+    if (inter_point.t2 < 0)
+        return (inter_point);
+    inter_point.t = (inter_point.t1 > 0 ? inter_point.t1 : inter_point.t2);
+    return (ft_find_edges(cylinder, ray, inter_point));
+}
+*/
+
+
+// coords, 3D ector, diameter, height, rgb
+// vector for cylinder needs to be used plus height
+bool	intersect_cylinder(t_ray *ray, t_objs *cylinder, t_obj_data *obj_data)
+{
+	t_vec3	oc;
+
+	oc = minus(ray->place, cylinder->center);
+
+	// obj_data->a = (dot_product(ray->vector, ray->vector)) - (pow(dot_product(ray->vector, cylinder->center), 2));
+
+	// obj_data->b = 2.0 * dot_product(ray->vector, cross_product(oc, cylinder->center));
+	// obj_data->c = dot_product(cross_product(oc, cylinder->center), cross_product(oc, cylinder->center)) - pow(cylinder->diameter / 2, 2);
+	// obj_data->d = pow(obj_data->b, 2) - 4 * obj_data->c * obj_data->a;
+	
+	obj_data->b = 2.0 * ray->place.x * ray->vector.x + 2 * ray->place.z * ray->vector.z;
+
+	obj_data->c = pow(ray->place.x, 2) + pow(ray->place.z, 2) - 1;
+	
+	obj_data->d = pow(obj_data->b, 2) - 4 * obj_data->c * obj_data->a;
+
+	if (obj_data->d < 0)
+		return (false); // no intersection
+	else
+	{
+		obj_data->t = (-obj_data->b + sqrt(obj_data->d)) / (2 * obj_data->a);
+		if (obj_data->t >= EPSILON)
+			return (true);
+
+		// obj_data->root1 = (-obj_data->b - sqrt(obj_data->d)) / (2.0 * obj_data->a);
+        // obj_data->root2 = (-obj_data->b + sqrt(obj_data->d)) / (2.0 * obj_data->a);
+        // if (obj_data->root1 < obj_data->root2)
+        //     obj_data->t = obj_data->root1;
+        // else
+        //     obj_data->t = obj_data->root2;
+        // return (true); // Intersection found
+	}
+	return (false);
+}
+
+
+JUST A WEIRD THINGY
+bool	intersect_cylinder(t_ray *ray, t_objs *cylinder, t_obj_data *obj_data)
+{
+	t_vec3	oc;
+	double	radius;
+
+	radius = cylinder->diameter / 2;
+	oc = minus(ray->place, cylinder->center);
+
+	obj_data->a = (dot_product(ray->vector, ray->vector)) - (pow(dot_product(ray->vector, cylinder->center), 2));
+	obj_data->b = 2.0 * dot_product(oc, ray->vector) - (dot_product(ray->vector, cylinder->center) * dot_product(oc, cylinder->center));
+	obj_data->c = dot_product(oc, oc) - pow(dot_product(oc, cylinder->center), 2) - pow(radius, 2);
+
+	// if (obj_data->a == 0 && obj_data->b != 0)
+	// {
+	// 	obj_data->root1 = -obj_data->c / obj_data->b;
+	// 	return (true);
+	// }
+	// else
+	// {
+	obj_data->d = obj_data->b * obj_data->b - 4 * obj_data->a * obj_data->c;
+	// obj_data->d = pow(obj_data->b, 2) - (4 * obj_data->a * obj_data->c);
+	if (obj_data->d < 0)
+		return (false); // no intersection
+	else
+	{
+		obj_data->root1 = (-obj_data->b - sqrt(obj_data->d)) / (2 * obj_data->a);
+		obj_data->root2 = (-obj_data->b + sqrt(obj_data->d)) / (2 * obj_data->a);
+		if (obj_data->root1 < obj_data->root2)
+			obj_data->t = obj_data->root1;
+		else
+			obj_data->t = obj_data->root2;
+		return (true); // Intersection found
+	}
+}
