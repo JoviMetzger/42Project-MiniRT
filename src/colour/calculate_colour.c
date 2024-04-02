a63b84edeb97d2aacdad1fc307667f0da4aaa6ea
smclacke <sarahmclacken@gmail.com>
2024-04-02 16:20:23 +0200

    calculate is weird now

--- src/colour/calculate_colour.c
+++ src/colour/calculate_colour.c
@@ -6,7 +6,7 @@
 /*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/03/08 16:05:21 by smclacke      #+#    #+#                 */
-/*   Updated: 2024/04/02 16:16:18 by smclacke      ########   odam.nl         */
+/*   Updated: 2024/04/02 16:17:58 by smclacke      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,17 +52,16 @@ uint32_t ft_calculate_colour(t_data *data, t_obj_data *obj_data, t_ray ray)
 		{
 			if (intersect_sphere(&ray, data->objs[i], obj_data))
 				colour = get_sphere_colour(data, obj_data, ray, data->objs[i]);
-				return (ft_convert_rgb(colour.r, colour.g, colour.b));
 		}
 		// else if (data->objs[i]->type == E_PLANE)
 		// {
+			// if (intersect_plane(&ray, data->objs[i], obj_data))
 		// 	colour = get_plane_colour(data, obj_data, ray, data->objs[i]);
-		// 	return (ft_convert_rgb(colour.r, colour.g, colour.b));
 		// }
 		// else if (data->objs[i]->type == E_CYLINDER)
 		// {
+			// if (intersect_cylinder(&ray, data->objs[i], obj_data))
 		// 	colour = get_cylinder_colour(data, obj_data, ray, data->objs[i]);
-		// 	return (ft_convert_rgb(colour.r, colour.g, colour.b));
 		// }
 		i++;
 		}
