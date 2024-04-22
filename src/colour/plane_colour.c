d997df6861bf05e6573775fc3cafc52025fd40b5
smclacke <sarahmclacken@gmail.com>
2024-04-22 21:28:38 +0200

    colour norming

--- src/colour/plane_colour.c
+++ src/colour/plane_colour.c
@@ -6,40 +6,71 @@
 /*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/04/02 15:44:57 by smclacke      #+#    #+#                 */
-/*   Updated: 2024/04/16 19:44:14 by smclacke      ########   odam.nl         */
+/*   Updated: 2024/04/22 21:27:03 by smclacke      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../header/miniRT.h"
 
+typedef	struct s_colour_vars
+{
+	t_colour	result;
+	double		ambient_intensity;
+	double		diffuse_intensity;
+	double		specular_intensity;
+	double		specular_power;
+	t_vec3		intersection_point;
+	t_vec3		normal;
+	double		ambient_red;
+	double		ambient_green;
+	double		ambient_blue;
+	double		diffuse_red;
+	double		diffuse_green;
+	double		diffuse_blue;
+	
+}			t_colour_vars;
+
 /**
- * @todo norm 
+ * @todo add comments/description here
+ */
+static void	init_vars(t_data *data, t_colour_vars *vars)
+{
+	vars->ambient_intensity = data->ambient.ratio; // (0.2)
+	vars->diffuse_intensity = data->light.ratio; // (0.6)
+	vars->specular_intensity = 0.2;
+	vars->specular_power = 32;
+	
+	// Ambient light contribution
+	vars->ambient_red = vars->ambient_intensity * data->ambient.colour.r;
+	vars->ambient_green = vars->ambient_intensity * data->ambient.colour.g;
+	vars->ambient_blue = vars->ambient_intensity * data->ambient.colour.b;
+}
+
+
+/**
+ * @todo add comments/description here
  */
 t_colour get_plane_colour(t_data *data, t_obj_data *obj_data, t_ray ray, t_objs *plane)
 {
-    t_colour result;
-    double AMBIENT_INTENSITY = data->ambient.ratio; // (0.2)
-    double DIFFUSE_INTENSITY = data->light.ratio;   // (0.6)
-    double SPECULAR_INTENSITY = 0.2;
-    double SPECULAR_POWER = 32;
-    
-    // Using the 'Phong reflection model'
-	t_vec3 intersection_point = plus(ray.place, mult_vecdub(ray.vector, obj_data->t));
-	t_vec3	normal = normalize_vector(minus(intersection_point, plane->center));
+    t_colour 		result;
+    t_colour_vars	vars;
 
-	// Ambient light contribution
-	double ambient_red = AMBIENT_INTENSITY * data->ambient.colour.r;
-	double ambient_green = AMBIENT_INTENSITY * data->ambient.colour.g;
-	double ambient_blue = AMBIENT_INTENSITY * data->ambient.colour.b;
+	init_vars(data, &vars);
+
+    // Using the 'Phong reflection model'
+	vars.intersection_point = plus(ray.place, mult_vecdub(ray.vector, obj_data->t));
+	vars.normal = normalize_vector(minus(vars.intersection_point, plane->center));
+
+
 
 	// Diffuse light contribution	
 	t_vec3 light_direction = normalize_vector(minus(data->light.place, intersection_point));
 	double diffuse_factor = dot_product(normal, light_direction);
 	if (diffuse_factor < 0.0)
 		diffuse_factor = 0.0;
-	double diffuse_red = DIFFUSE_INTENSITY * diffuse_factor * data->light.colour.r;
-	double diffuse_green = DIFFUSE_INTENSITY * diffuse_factor * data->light.colour.g;
-	double diffuse_blue = DIFFUSE_INTENSITY * diffuse_factor * data->light.colour.b;
+	vars.diffuse_red = vars.diffuse_intensity * diffuse_factor * data->light.colour.r;
+	vars.diffuse_green = vars.diffuse_intensity * diffuse_factor * data->light.colour.g;
+	vars.diffuse_blue = vars.diffuse_intensity * diffuse_factor * data->light.colour.b;
 	
 	// Specular light contribution
 	t_vec3 view_direction = normalize_vector(minus(ray.place, intersection_point));
