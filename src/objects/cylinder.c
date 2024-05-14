8db19a9d04fcb6bc800b66c0e0a101bdf0b47a59
smclacke <sarahmclacken@gmail.com>
2024-05-14 15:26:26 +0200

    test uncapped cyl more to triple check its ok, then dont touch and just try add cap

--- src/objects/cylinder.c
+++ src/objects/cylinder.c
@@ -6,7 +6,7 @@
 /*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/03/07 19:29:03 by smclacke      #+#    #+#                 */
-/*   Updated: 2024/05/14 14:53:28 by smclacke      ########   odam.nl         */
+/*   Updated: 2024/05/14 15:25:53 by smclacke      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,10 +37,11 @@ bool	check_roots(t_obj_data *obj, t_objs *cyl, t_ray *ray)
 	
 	if (obj->root1 < 0 && obj->root2 < 0)
 		return (false);
-	if ((obj->root2 < obj->root1 && obj->root2 > 0) || (obj->root1 < obj->root2 && obj->root1 <= 0)) 
-		obj->t = obj->root1;
+	if ((obj->root2 < obj->root1 && obj->root2 > 0)
+		|| (obj->root1 < obj->root2 && obj->root1 <= 0)) 
+		obj->t = obj->root2; // of andersom
 	else
-		obj->t = obj->root2;
+		obj->t = obj->root1;
 
 	if (obj->t > 0)
 	{
@@ -51,82 +52,80 @@ bool	check_roots(t_obj_data *obj, t_objs *cyl, t_ray *ray)
 }
 
 
-// infinity shittt
-// double	plane_cyl(t_obj_data *obj, t_ray *ray, t_vec3 center, t_vec3 vector)
+// // infinity shittt
+// // double	plane_cyl(t_obj_data *obj, t_ray *ray, t_vec3 center, t_vec3 vector)
+// // {
+// // 	t_vec3 oc;
+// // 	double	denom;
+
+// // 	denom = dot_product(ray->vector, vector);
+// // 	if (fabs(denom) > EPSILON)
+// // 	{
+// // 		oc = minus(ray->place, center);
+// // 		obj->t = -dot_product(oc, vector) / denom;
+// // 		if (obj->t >= EPSILON)
+// // 			return (check_closest(obj));
+// // 		else
+// // 			return (INFINITY);
+// // 	}
+// // 	return (INFINITY);
+// // }
+
+// double	plane_cyl(t_ray *ray, t_vec3 center, t_vec3 vector)
 // {
-// 	t_vec3 oc;
+// 	double	x;
 // 	double	denom;
 
-// 	denom = dot_product(ray->vector, vector);
-// 	if (fabs(denom) > EPSILON)
-// 	{
-// 		oc = minus(ray->place, center);
-// 		obj->t = -dot_product(oc, vector) / denom;
-// 		if (obj->t >= EPSILON)
-// 			return (check_closest(obj));
-// 		else
-// 			return (INFINITY);
-// 	}
-// 	return (INFINITY);
+// 	denom = dot_product(vector, ray->vector);
+// 	if (denom == 0)
+// 		return (INFINITY);
+// 	x = -dot_product(vector, minus(ray->place, center)) / denom; // min?
+// 	return (x > 0 ? x : INFINITY);
 // }
 
-double	plane_cyl(t_ray *ray, t_vec3 center, t_vec3 vector)
-{
-	double	x;
-	double	denom;
 
-	denom = dot_product(vector, ray->vector);
-	if (denom == 0)
-		return (INFINITY);
-	x = -dot_product(vector, minus(ray->place, center)) / denom; // min?
-	return (x > 0 ? x : INFINITY);
-}
+// bool	check_caps(t_obj_data *obj, t_objs *cyl, t_ray *ray)
+// {
+// 	double hit1;
+// 	double hit2;
+// 	t_vec3 pnt1;
+// 	t_vec3 pnt2;
+// 	t_vec3 cent2;
 
+// 	cent2 = plus(cyl->center, mult_vecdub(cyl->vector, cyl->height));
 
-bool	check_caps(t_obj_data *obj, t_objs *cyl, t_ray *ray)
-{
-	double hit1;
-	double hit2;
-	t_vec3 pnt1;
-	t_vec3 pnt2;
-	t_vec3 cent2;
-
-	cent2 = plus(cyl->center, mult_vecdub(cyl->vector, cyl->height));
-
-	hit1 = plane_cyl(ray, cyl->center, cyl->vector);
-	hit2 = plane_cyl(ray, cent2, cyl->vector);
-	if (hit1 < INFINITY || hit2 < INFINITY)
-	{
-		pnt2 = plus(mult_vecdub(ray->vector, hit2), ray->place);
-		pnt1 = plus(mult_vecdub(ray->vector, hit1), ray->place);
-		// pnt2 = plus(ray->place, mult_vecdub(ray->vector, hit2));
-		// pnt1 = plus(ray->place, mult_vecdub(ray->vector, hit1));
+// 	hit1 = plane_cyl(ray, cyl->center, cyl->vector);
+// 	hit2 = plane_cyl(ray, cent2, cyl->vector);
+// 	if (hit1 < INFINITY || hit2 < INFINITY)
+// 	{
+// 		pnt2 = plus(mult_vecdub(ray->vector, hit2), ray->place);
+// 		pnt1 = plus(mult_vecdub(ray->vector, hit1), ray->place);
 		
-		// not sure this bit is necessary.. doesnt seem to ever hit?
-		// if ((hit1 < INFINITY && distance(pnt1, cyl->center) <= obj->radius) 
-		// 	&& (hit2 < INFINITY && distance(pnt2, cent2) <= obj->radius))
-		// {
-		// 	// printf("this hit\n");/
-		// 	if (hit1 < hit2) // more or less?
-		// 		obj->t = hit1;
-		// 	else
-		// 		obj->t = hit2;
-		// 	return (true);
-		// }
-		if (hit1 < INFINITY && distance(pnt1, cyl->center) <= obj->radius)
-		{
-			obj->t = hit1;
-			return (true);
-		}
-		else if (hit2 < INFINITY && distance(pnt2, cent2) <= obj->radius)
-		{
-			obj->t = hit2;
-			return (true);
-		}
-		return (false);
-	}
-	return (false);
-}
+// 		// not sure this bit is necessary.. doesnt seem to ever hit?
+// 		// if ((hit1 < INFINITY && distance(pnt1, cyl->center) <= obj->radius) 
+// 		// 	&& (hit2 < INFINITY && distance(pnt2, cent2) <= obj->radius))
+// 		// {
+// 		// 	// printf("this hit\n");/
+// 		// 	if (hit1 < hit2) // more or less?
+// 		// 		obj->t = hit1;
+// 		// 	else
+// 		// 		obj->t = hit2;
+// 		// 	return (true);
+// 		// }
+// 		if (hit1 < INFINITY && distance(pnt1, cyl->center) <= obj->radius)
+// 		{
+// 			obj->t = hit1;
+// 			return (true);
+// 		}
+// 		else if (hit2 < INFINITY && distance(pnt2, cent2) <= obj->radius)
+// 		{
+// 			obj->t = hit2;
+// 			return (true);
+// 		}
+// 		return (false);
+// 	}
+// 	return (false);
+// }
 
 bool	intersect_cylinder(t_ray *ray, t_objs *cyl, t_obj_data *obj)
 {
@@ -148,16 +147,19 @@ bool	intersect_cylinder(t_ray *ray, t_objs *cyl, t_obj_data *obj)
 		{	
 			if (check_roots(obj, cyl, ray) == true)
 			{
-				obj->tmp_t = obj->t;
-				if (check_caps(obj, cyl, ray) == true)
+				// obj->tmp_t = obj->t;
+				// if (check_caps(obj, cyl, ray) == true)
+				// 	return (check_closest(obj));
+				// else
+				// {
+				// 	// return (false);
+				// 	obj->t = obj->tmp_t;
+				// 	return (check_closest(obj));
+				// }
 					return (check_closest(obj));
-				else
-				{
-					obj->t = obj->tmp_t;
-					return (check_closest(obj));
-				}
 			}
 		}
 	}
 	return (false);
 }
+
