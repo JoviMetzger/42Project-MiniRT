fb95baa5f59bc476ec6fccbe8faf76c69e519ed4
smclacke <sarahmclacken@gmail.com>
2024-05-21 15:06:58 +0200

    notes, bit by bit, start by organsing structs a bit better

--- src/colour/get_colour.c
+++ src/colour/get_colour.c
@@ -6,7 +6,7 @@
 /*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/04/02 15:45:05 by smclacke      #+#    #+#                 */
-/*   Updated: 2024/04/30 16:04:38 by smclacke      ########   odam.nl         */
+/*   Updated: 2024/05/21 15:05:50 by smclacke      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,7 +85,7 @@ t_colour	get_colour(t_data *data, t_obj_data *obj, t_ray ray, t_objs *obj_i)
 	vars.spec_intensity = 0.2;
 	vars.spec_power = 32;
 	vars.inter_point = plus(ray.place, mult_vecdub(ray.vector, obj->t));
-	vars.normal = normalize_vector(minus(vars.inter_point, obj_i->center));
+	vars.normal = normalize_vector(minus(vars.inter_point, obj_i->center)); // !!! only for sphere
 	light_funcs(data, &vars, &ray);
 	vars.final_red = vars.ambient_red + vars.diffuse_red;
 	vars.final_red += vars.spec_red;
