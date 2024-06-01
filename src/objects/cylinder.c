907afabf148b360861dec43fa037f7816b2edd38
smclacke <sarahmclacken@gmail.com>
2024-06-01 20:35:07 +0200

    sa.rt has all the shizzle

--- src/objects/cylinder.c
+++ src/objects/cylinder.c
@@ -6,7 +6,7 @@
 /*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/03/07 19:29:03 by smclacke      #+#    #+#                 */
-/*   Updated: 2024/06/01 20:19:16 by smclacke      ########   odam.nl         */
+/*   Updated: 2024/06/01 20:34:54 by smclacke      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
