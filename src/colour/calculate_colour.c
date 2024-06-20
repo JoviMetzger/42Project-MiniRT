9ae90ee0391ab84dcc94e7c468ac8d4241774050
smclacke <sarahmclacken@gmail.com>
2024-06-20 14:44:06 +0200

    white borken...

--- src/colour/calculate_colour.c
+++ src/colour/calculate_colour.c
@@ -6,7 +6,7 @@
 /*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/03/08 16:05:21 by smclacke      #+#    #+#                 */
-/*   Updated: 2024/06/16 20:55:41 by smclacke      ########   odam.nl         */
+/*   Updated: 2024/06/20 14:33:14 by smclacke      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ static void	get_ret(t_data *data, t_hit_data *hit, t_objs *obj, int i)
 		data->pix[i]->ambient = get_ambient(data, obj);
 		// idea but fuck knows...
 		// need somehow to get light after checking all lights and add most light to pixel->light
-		data->pix[i]->light = get_light(data, data->ray, obj);
+		data->pix[i]->colour = get_light(data, data->ray, obj);
 	}
 	else
 		data->pix[i]->colour = data->pix[i]->black; // didnt hit anything
--- src/window/do_calcs.c
+++ src/window/do_calcs.c
@@ -6,44 +6,16 @@
 /*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/06/16 16:14:41 by smclacke      #+#    #+#                 */
-/*   Updated: 2024/06/16 20:54:52 by smclacke      ########   odam.nl         */
+/*   Updated: 2024/06/20 14:33:04 by smclacke      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../header/miniRT.h"
 
-// static t_ray	init_lights(t_data *data, t_ray	light_ray, t_objs *obj, int index)
-// {
-// 	ft_bzero(&light_ray, sizeof(t_ray));
-// 	data->vars.curr_light = data->light[index];
-// 	data->vars.intersect_p = plus(data->ray.place, mult_vecdub(data->ray.vector, obj->obj_t));
-// 	data->vars.light_dir = normalize(minus(data->vars.curr_light->place,
-// 		data->vars.intersect_p));
-// 	data->vars.normal = obj->normal;
-// 	// light_ray.place = data->light[index]->place;
-// 	light_ray.place = plus(data->vars.intersect_p, mult_vecdub(data->vars.normal, EPSILON));
-// 	light_ray.vector = data->vars.light_dir;
-// 	return (light_ray);
-// }
-
-// static void	init_light(t_data *data, t_objs *obj, double hit)
-// {
-// 	data->vars.curr_light = data->light[0]; // !!!!!!
-// 	data->vars.intersect_p = plus(data->ray.place, mult_vecdub(data->ray.vector, hit));
-// 	data->vars.light_dir = normalize(minus(data->vars.curr_light->place,
-// 		data->vars.intersect_p));
-// 	data->vars.normal = obj->normal;
-// }
-
 void	do_calcs(t_data *data)
 {
 	t_hit_data	hit;
 	int			i = 0;
-
-	// t_ray		light_ray;
-	// light_ray = init_lights(data, light_ray, data->pix[i]->obj, index);
-
-	// int			index = 0;
 
 	while (i < data->total_pix)
 	{
@@ -54,19 +26,15 @@ void	do_calcs(t_data *data)
 		data->pix[i]->hit_t = hit.t;
 		i++;
 	}
-	i = 0;
-	while (i < data->total_pix)
-	{
-		if (data->pix[i]->hit_b == true)
-		{
-			data->pix[i]->colour = data->pix[i]->ambient;
-			// while (index < data->light_i)
-			// {
-				// if light[index] [DIRECTLY] intersects object, overwrite colour with light
-					data->pix[i]->colour = data->pix[i]->light;
-			// 	index++;
-			// }
-		}
-		i++;
-	}
+	// i = 0;
+	// while (i < data->total_pix)
+	// {
+	// 	if (data->pix[i]->hit_b == true)
+	// 	{
+	// 		data->pix[i]->colour = data->pix[i]->light;
+	// 		// if in shadow
+	// 			// data->pix[i]->colour = data->pix[i]->ambient;
+	// 	}
+	// 	i++;
+	// }
 }
