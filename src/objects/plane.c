8f8d301f6a9f968a37308c5756e582aaee54000b
Jovanka Metzger <jmetzger@f1r1s6.codam.nl>
2024-08-16 16:56:35 +0200

    norm

--- src/objects/plane.c
+++ src/objects/plane.c
@@ -6,7 +6,7 @@
 /*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/03/07 19:29:03 by smclacke      #+#    #+#                 */
-/*   Updated: 2024/08/16 14:57:05 by jmetzger      ########   odam.nl         */
+/*   Updated: 2024/08/16 14:59:27 by jmetzger      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,6 @@ bool intersect_plane(t_ray *ray, t_objs *plane,  t_hit_data *hit)
 		{
 			if (denom > 0)
 				plane->normal = mult_vecdub(plane->vector, -1);
-				
 			plane->hit_pos = plus(ray->place, mult_vecdub(ray->vector, plane->obj_t));
 			plane->obj_t = hit->t;
 			return (true);
