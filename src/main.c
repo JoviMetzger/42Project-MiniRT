f388b7fde6b80bc0567fa49af3a90b4ea9d39cd5
smclacke <sarahmclacken@gmail.com>
2024-08-19 19:30:38 +0200

    eval files

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/03/07 19:29:03 by smclacke      #+#    #+#                 */
-/*   Updated: 2024/08/16 17:58:39 by smclacke      ########   odam.nl         */
+/*   Updated: 2024/08/19 19:30:26 by smclacke      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
